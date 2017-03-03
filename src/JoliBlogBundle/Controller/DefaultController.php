43245ff5a5bceeb5debac6fec36e636e0384ba25
Mathieu Santostefano <msantostefano@jolicode.com>
2017-03-03 17:37:48 +0100

    🚧  Exercice corrigé

--- src/JoliBlogBundle/Controller/DefaultController.php
+++ src/JoliBlogBundle/Controller/DefaultController.php
@@ -4,14 +4,34 @@ namespace JoliBlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/{name}", name="ma_jolie_route")
+     * @param string $name
+     * @return Response
      */
-    public function indexAction()
+    public function indexAction(string $name): Response
     {
-        return $this->render('JoliBlogBundle:Default:index.html.twig');
+        $router = $this->get('router');
+        $url = $router->generate('ma_jolie_route', ['name' => $name], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        switch ($name){
+            case 'facebook':
+            case 'twitter':
+            case 'google':
+                return $this->redirect("http://".$name.".fr");
+            break;
+
+            default:
+                return $this->render('JoliBlogBundle:Default:index.html.twig', [
+                    'name' => $name,
+                    'url' => $url,
+                ]);
+            break;
+        }
     }
 }
